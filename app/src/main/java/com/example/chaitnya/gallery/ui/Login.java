de3f0e8deda268b9cf3efc88bc1e2779d2dcacf9
Eventodb <hiteshd101@gmail.com>
2017-01-04 14:48:45 +0530

    Navigation Drawer Grid fragment and grid_ke_uppar_ki_xml not copied

--- app/src/main/java/com/example/chaitnya/gallery/ui/Login.java
+++ app/src/main/java/com/example/chaitnya/gallery/ui/Login.java
@@ -2,6 +2,7 @@ package com.example.chaitnya.gallery.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,7 @@ import android.widget.Button;
 import com.example.chaitnya.gallery.MainActivity;
 import com.example.chaitnya.gallery.R;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
